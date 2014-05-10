2d71df0b1d938ac4c9cbf6c2af9c3588a4081234
alsmadi <alsmadi@gmail.com>
2014-05-10 18:30:20 +0300

    first commit

---
+++ is_351_demo/IS_351_Demo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package is_351_demo;
+
+/**
+ *
+ * @author Izzat
+ */
+public class IS_351_Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("Add your class and make a link to it");
+    }
+}
